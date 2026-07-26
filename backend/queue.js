eddb30cdeb06153ede039b9e4dd5f5b201279e05
Pavi Gupta <pavigupta21@gmail.com>
2026-07-26 15:12:02 +0530

    Support Upstash Redis

--- backend/queue.js
+++ backend/queue.js
@@ -1,27 +1,28 @@
 const { Queue } = require("bullmq");
 const IORedis = require("ioredis");
 
-const connection = new IORedis({
-    host: "127.0.0.1",
-    port: 6379,
-    maxRetriesPerRequest: null
-});
+const connection = process.env.REDIS_URL
+    ? new IORedis(process.env.REDIS_URL, {
+          maxRetriesPerRequest: null,
+      })
+    : new IORedis({
+          host: "127.0.0.1",
+          port: 6379,
+          maxRetriesPerRequest: null,
+      });
 
-const faceQueue = new Queue(
-    "face-processing",
-    {
-        connection,
-        defaultJobOptions: {
-            attempts: 2,
-            backoff: {
-                type: "exponential",
-                delay: 1000
-            }
-        }
-    }
-);
+const faceQueue = new Queue("face-processing", {
+    connection,
+    defaultJobOptions: {
+        attempts: 2,
+        backoff: {
+            type: "exponential",
+            delay: 1000,
+        },
+    },
+});
 
 module.exports = {
     faceQueue,
-    connection
+    connection,
 };
